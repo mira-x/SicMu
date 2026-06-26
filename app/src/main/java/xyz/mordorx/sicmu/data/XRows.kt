d92fccb7fad1fa88e061a7d71e89dff2774fec03
Mira Alpmann <mira@alpmann.ch>
2026-06-26 14:50:48 +0200

    XRows: speed up init by ~250ms

--- app/src/main/java/xyz/mordorx/sicmu/data/XRows.kt
+++ app/src/main/java/xyz/mordorx/sicmu/data/XRows.kt
@@ -15,9 +15,9 @@ import kotlin.io.path.name
 import kotlin.io.path.pathString
 
 object XRows {
-    public lateinit var appContext: Context
+    lateinit var appContext: Context
 
-    public var rowsFolded = MutableStateFlow<List<Row>>(emptyList())
+    var rowsFolded = MutableStateFlow<List<Row>>(emptyList())
         private set
 
     private var rowsUnfolded = MutableStateFlow<List<Row>>(emptyList())
@@ -26,7 +26,7 @@ object XRows {
 
     }
 
-    public fun initRowsFromMediaStore() = CoroutineScope(Dispatchers.IO).launch {
+    fun initRowsFromMediaStore() = CoroutineScope(Dispatchers.IO).launch {
         // First, we add all songs. Then we add groups/folders
 
         val startTime = System.currentTimeMillis()
@@ -101,9 +101,12 @@ object XRows {
                 folders.add(relativePath)
             }
         }
+        Log.d("XRows-Time", "MediaStore read took ${System.currentTimeMillis()-startTime} Ms")
 
         // This is necessary for easy insertion of folders/groups later
         rows.sortBy { row -> (row as XRowSong).path }
+
+        Log.d("XRows-Time", "Sorting took ${System.currentTimeMillis()-startTime} Ms")
 
         /**
          * Isolate all folders (even those without song files in them) along the hierarchy.
@@ -120,19 +123,26 @@ object XRows {
                 }
             }
         })
+        Log.d("XRows-Time", "Loading folders took ${System.currentTimeMillis()-startTime} Ms")
 
+        /**
+         * For each folder, sum up statistics and create XRowGroup objects.
+         */
         val groups = folders.sortedBy { str -> str }.mapNotNull {
             val p = Path(it)
             val displayName = p.name
             val containingFolder = p.parent?.pathString ?: return@mapNotNull null
             val fullPath = p.pathString
             val level = it.slashCount
-            // TODO: Add SortedList() class/type that finds upper and lower bound of path match, using binary search.
-            val songsContained = rows
-                .filter { r -> (r as XRowSong).path.startsWith(fullPath) }
-
-            val songCount = songsContained.size
-            val playtimeMs = songsContained.sumOf { song -> (song as XRowSong).durationMs }
+            // This sums up all child songs' numbers. Benchmarks show that using drop/take is
+            // faster than binary search for the first/last relevant XRowSong (i.e. 9ms vs 21ms).
+            // I suppose that's CPU branch prediction.
+            val (playtimeMs, songCount) = rows
+                .asSequence()
+                .dropWhile { r -> !(r as XRowSong).path.startsWith(fullPath) }
+                .takeWhile { r -> (r as XRowSong).path.startsWith(fullPath) }
+                .map { r -> Pair((r as XRowSong).durationMs, 1) }
+                .reduce { acc, row -> Pair(acc.first + row.first, acc.second + row.second) }
 
             XRowGroup(
                 level = level,
@@ -144,7 +154,11 @@ object XRows {
                 durationMs = playtimeMs
             )
         }
+        Log.d("XRows-Time", "Creating folders took ${System.currentTimeMillis()-startTime} Ms")
 
+        /**
+         * Insert XRowGroup's at appropriate spaces
+         */
         groups.forEach { rowGroup ->
             val above = rows.withIndex().first { row -> row.value.path.startsWith(rowGroup.path) }
 
@@ -159,9 +173,10 @@ object XRows {
 
             rows.add(above.index, rowGroup)
         }
+        Log.d("XRows-Time", "Inserting folders took ${System.currentTimeMillis()-startTime} Ms")
 
         // Log them all
-        rows.forEach { finalRow ->
+        /*rows.forEach { finalRow ->
             val pad = " ".repeat(finalRow.level)
             if (finalRow is XRowGroup) {
                 val g = finalRow as XRowGroup
@@ -183,11 +198,7 @@ object XRows {
                   | AUD-20260319-WA0001.opus (0:12) |
                   ...
              */
-        }
-
-        val finishMs = System.currentTimeMillis()
-        val dur = finishMs - startTime
-        Log.d("XRows-Finale-Time", "scan took $dur Ms")
+        }*/
 
     }
 }
